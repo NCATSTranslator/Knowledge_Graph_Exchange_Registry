c6b42141c3f01248dc228c2817d759a4e94e182b
Kenneth Bruskiewicz <kenneth.bruskiewicz@gmail.com>
2021-01-28 18:07:00 -0800

    refining upload process

--- kgea/server/openapi_server/controllers/upload_controller.py
+++ kgea/server/openapi_server/controllers/upload_controller.py
@@ -42,7 +42,7 @@ def get_upload_form():  # noqa: E501
     return page
 
 
-def register_file_set(submitter, kg_name):  # noqa: E501
+def register_file_set(body):  # noqa: E501
     """Register web form details specifying a KGE File Set location
 
      # noqa: E501
@@ -54,7 +54,40 @@ def register_file_set(submitter, kg_name):  # noqa: E501
 
     :rtype: str
     """
-    print(submitter, kg_name)
+    submitter = body['submitter']
+    kg_name = body['kg_name']
+
+    object_location = Template('$DIRECTORY_NAME/$KG_NAME/').substitute(
+        DIRECTORY_NAME='kge-data', 
+        KG_NAME=Path(data_file_content.filename).stem
+    )
+    
+    def location_available(bucket_name, object_key):
+        """
+        Guarantee that we can write to the location of the object without overriding everything
+
+        :param bucket: The bucket
+        :param object: The object in the bucket
+        :return: True if the object is not in the bucket, False if it is already in the bucket
+        """
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(bucket_name)
+        key = object_key
+        objs = list(bucket.objects.filter(Prefix=key))
+        if any([w.key == path_s3 for w in objs]):
+            # exists
+            # invert because unavailable
+            return not True
+        else:
+            # doesn't exist
+            # invert because available
+            return not False
+
+    def register_smartapi():
+        # using https://github.com/NCATS-Tangerine/translator-api-registry
+        
+        return True
+
     return 'do some magic!'
 
 
@@ -75,13 +108,12 @@ def upload_file_set(kg_name, data_file_content, data_file_metadata=None):  # noq
 
     print(kg_name, data_file_content, data_file_metadata)
 
-    object_location = Template('$DIRECTORY_NAME/$KG_NAME/$CONTENT_TYPE/').substitute(
+    object_location = Template('$DIRECTORY_NAME/$KG_NAME/').substitute(
         DIRECTORY_NAME='kge-data', 
-        CONTENT_TYPE='content'
-        KG_NAME=Path(data_file_content.filename).stem
+        KG_NAME=kg_name
     )
 
-    def upload_file(data_file_content, bucket, object_name, override=False):
+    def upload_file(data_file, bucket_name, object_location, content_type, override=False):
         """Upload a file to an S3 bucket
 
         :param file_name: File to upload
@@ -89,6 +121,9 @@ def upload_file_set(kg_name, data_file_content, data_file_metadata=None):  # noq
         :param object_name: S3 object name. If not specified then file_name is used
         :return: True if file was uploaded, else False
         """
+
+        def registered(kg_name):
+            pass
 
         def location_available(bucket_name, object_key):
             """
@@ -111,13 +146,14 @@ def upload_file_set(kg_name, data_file_content, data_file_metadata=None):  # noq
                 # invert because available
                 return not False
 
+        object_key = object_location + content_type + '/' + data_file
+
         # Upload the file
         s3_client = boto3.client('s3', config=Config(region_name='ca-central-1', signature_version='s3v4'))
-        # TODO: change location available to work better with "register"
-        if location_available(bucket, object_name) or override:
+        if api_registered(kg_name) and not location_available(bucket_name, object_location) or override:
             try:
-                with data_file_content.stream as f:
-                    s3_client.upload_fileobj(f, bucket, object_name)
+                with data_file.stream as f:
+                    s3_client.upload_fileobj(f, bucket_name, object_name)
             except ClientError as e:
                 print(e)
                 return False
@@ -125,19 +161,17 @@ def upload_file_set(kg_name, data_file_content, data_file_metadata=None):  # noq
         else:
             return False
     
-    # TODO
     def register_smartapi():
+        # using https://github.com/NCATS-Tangerine/translator-api-registry
         return True
 
-    maybeUploadContent = upload_file(data_file_content, bucket="star-ncats-translator", object_name=object_location+"content/"+data_file_content.filename)
-    maybeUploadMetaData = True #or upload_file(data_file_metadata, bucket_name="star-ncats-translator", object_name=object_location+"metadata/"+data_file_metadata.filename)
-    maybeRegisterSmartAPI = register_smartapi()
-    if maybeUploadContent and maybeUploadMetaData and maybeRegisterSmartAPI:
-        # DONE: "Authorization mechanism not provided" Error
-        # CANCEL: link to resources? DEEPLINK or API LINK?
-            # TODO: Configuration for Hostname?
-        # return create_presigned_url(bucket="star-ncats-translator", object_name=object_location+"content/"+data_file_content.filename) 
-        return "Success!" #TODO: Success Page
+    maybeUploadContent = upload_file(data_file_content, bucket_name="star-ncats-translator", object_location=object_location, content_type="content") 
+    maybeUploadMetaData = None or upload_file(data_file_metadata, bucket_name="star-ncats-translator", object_location=object_location, content_type="metadata")
+    
+    if maybeUploadContent or maybeUploadMetaData:
+        return {
+            [data_file_content.filename]: maybeUploadContent
+            [data_file_metadata.filename]: maybeUploadMetaData
+        }
     else:
-        # TODO: how to handle failure? Website Redirect?
-        return "Failure!" #, (maybeUploadContent, maybeUploadMetaData, maybeRegisterSmartAPI)
+        return "Failure!"
