80c2c7901d59ddf76bd1d436cfabafa918a6418b
Richard Bruskiewich <richard@starinformatics.com>
2021-02-04 23:52:28 -0800

    tribbleshooting

--- kgea/server/openapi_server/controllers/upload_controller.py
+++ kgea/server/openapi_server/controllers/upload_controller.py
@@ -34,7 +34,7 @@ def get_upload_form(kg_name, session):  # noqa: E501
     return get_kge_upload_form(kg_name, session)
 
 
-def register_file_set(session, submitter, kg_name):  # noqa: E501
+def register_file_set(session=None, submitter=None, kg_name=None, **kwargs):  # noqa: E501
     """Register core parameters for the KGE File Set upload
 
      # noqa: E501
@@ -48,10 +48,11 @@ def register_file_set(session, submitter, kg_name):  # noqa: E501
 
     :rtype: str
     """
+    print("register_file_set: "+str(locals()))
     return register_kge_file_set(session, submitter, kg_name)
 
 
-def upload_file_set(kg_name, session, data_file_content, data_file_metadata=None):  # noqa: E501
+def upload_file_set(kg_name=None, session=None, data_file_content=None, data_file_metadata=None):  # noqa: E501
     """Upload web form details specifying a KGE File Set upload process
 
      # noqa: E501
