ff34a801ecb2bd48e9213a8e2e426f1e7a5dfd5d
Richard Bruskiewich <richard@starinformatics.com>
2021-02-18 08:26:47 -0800

    fixed upload controller stubs

--- kgea/server/openapi_server/controllers/upload_controller.py
+++ kgea/server/openapi_server/controllers/upload_controller.py
@@ -1,7 +1,9 @@
-import connexion
-import six
-
-from openapi_server import util
+from ..kgea_handlers import (
+    get_kge_file_upload_form,
+    get_kge_registration_form,
+    register_kge_file_set,
+    upload_kge_file,
+)
 
 
 def get_file_upload_form(session, submitter, kg_name):  # noqa: E501
@@ -18,7 +20,7 @@ def get_file_upload_form(session, submitter, kg_name):  # noqa: E501
 
     :rtype: str
     """
-    return 'do some magic!'
+    return get_kge_file_upload_form(session, submitter, kg_name)
 
 
 def get_registration_form(session):  # noqa: E501
@@ -31,40 +33,30 @@ def get_registration_form(session):  # noqa: E501
 
     :rtype: str
     """
-    return 'do some magic!'
+    return get_kge_registration_form(session)
 
 
-def register_file_set(session, submitter, kg_name):  # noqa: E501
+def register_file_set(body):  # noqa: E501
     """Register core parameters for the KGE File Set upload
 
      # noqa: E501
 
-    :param session: 
-    :type session: str
-    :param submitter: 
-    :type submitter: str
-    :param kg_name: 
-    :type kg_name: str
+    :param body:
+    :type body: dict
 
     :rtype: str
     """
-    return 'do some magic!'
+    return register_kge_file_set(body)
 
 
-def upload_file(session, upload_mode, content_url=None, content_file=None):  # noqa: E501
+def upload_file(body):  # noqa: E501
     """Upload processing of KGE File Set file
 
      # noqa: E501
 
-    :param session: 
-    :type session: str
-    :param upload_mode: 
-    :type upload_mode: str
-    :param content_url: 
-    :type content_url: str
-    :param content_file: 
-    :type content_file: str
+    :param body:
+    :type body: dict
 
     :rtype: str
     """
-    return 'do some magic!'
+    return upload_kge_file(body)
