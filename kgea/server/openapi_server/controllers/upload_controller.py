6c5d1d4c3b37308146aae6e872513e6946bac5f6
Richard Bruskiewich <richard@starinformatics.com>
2021-03-08 17:43:01 -0800

    Remove UI  elements from 'server' web  services API and associated  handlers

--- kgea/server/openapi_server/controllers/upload_controller.py
+++ kgea/server/openapi_server/controllers/upload_controller.py
@@ -1,40 +1,9 @@
 from aiohttp import web
 
 from ..kgea_handlers import (
-    get_kge_file_upload_form,
-    get_kge_registration_form,
     register_kge_file_set,
     upload_kge_file,
 )
-
-
-async def get_file_upload_form(request: web.Request, session: str, submitter: str, kg_name: str) -> web.Response:
-    """Get web form for the KGE File Set upload process
-
-    
-    :param request:
-    :type request: web.Request
-    :param session: 
-    :type session: str
-    :param submitter: 
-    :type submitter: str
-    :param kg_name: 
-    :type kg_name: str
-
-    """
-    return await get_kge_file_upload_form(request, session, submitter, kg_name)
-
-
-async def get_registration_form(request: web.Request, session: str) -> web.Response:
-    """Prompt user for core parameters of the KGE File Set upload
-
-    :param request:
-    :type request: web.Request
-    :param session:
-    :type session: str
-
-    """
-    return await get_kge_registration_form(request, session)
 
 
 async def register_file_set(request: web.Request) -> web.Response:
--- kgea/server/openapi_server/kgea_handlers.py
+++ kgea/server/openapi_server/kgea_handlers.py
@@ -44,180 +44,10 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-#############################################################
-# Site Controller Handlers
-#
-# Insert imports and return calls into site_controller.py:
-#
-# from ..kge_handlers import (
-#     kge_landing_page,
-#     kge_login,
-#     kge_client_authentication,
-#     get_kge_home,
-#     kge_logout
-# )
-#############################################################
-
 # This is the home page path,
 # should match the API path spec
 LANDING = '/'
 HOME = '/home'
-
-
-# @aiohttp_jinja2.template('login.html')
-def kge_landing_page(request: web.Request, session_id=None):  # noqa: E501
-    """Display landing page.
-
-     # noqa: E501
-
-    :param request:
-    :type request: web.Request
-    :param session_id:
-    :type session_id: str
-
-    :rtype: str
-    """
-    # validate the session key
-    if valid_session(session_id):
-        # then redirect to an authenticated home page
-        authenticated_url = HOME + '?session=' + session_id
-        raise web.HTTPFound(authenticated_url)
-    else:
-        # If session is not active, then
-        # render login page (no Jinja parameterization)
-        # return {}
-
-        context = {}
-        response = aiohttp_jinja2.render_template('login.html',
-                                                  request,
-                                                  context)
-        return response
-
-
-
-@aiohttp_jinja2.template('home.html')
-async def get_kge_home(request: web.Request, session_id: str = None):  # noqa: E501
-    """Get default landing page
-
-     # noqa: E501
-
-    :param request:
-    :type request: web.Request
-    :param session_id:
-    :type session_id: str
-
-    :rtype: web.Response
-    """
-
-    # validate the session key
-    if valid_session(session_id):
-        return {"session":  session_id}
-    else:
-        # If session is not active, then just
-        # redirect back to unauthenticated landing page
-        raise web.HTTPFound(LANDING)
-
-
-# hack: short term dictionary
-_state_cache = []
-
-
-async def kge_client_authentication(request: web.Request, code: str, state: str):  # noqa: E501
-    """Process client authentication
-
-     # noqa: E501
-
-    :param request:
-    :type request: web.Request
-    :param code:
-    :type code: str
-    :param state:
-    :type state: str
-    """
-
-    # Establish session here if there
-    # is a valid access code & state variable?
-    if state in _state_cache:
-
-        # state 'secrets' are only got for one request
-        _state_cache.remove(state)
-
-        # now, check the returned code for authorization
-        if code:
-
-            # TODO: check AWS authorization token
-
-            # Let everything through for the initial iteration
-            # Need to tie into AWS Cognito validation
-            authenticated = True
-
-            if authenticated:
-                # create and persist Session here
-                session_id = create_session()
-
-                # then redirect to an authenticated home page
-                authenticated_url = HOME + '?session=' + session_id
-                raise web.HTTPFound(authenticated_url)
-
-    # If authentication conditions are not met, then
-    # simply redirect back to public landing page
-    raise web.HTTPFound(LANDING)
-
-
-async def kge_login(request: web.Request):  # noqa: E501
-    """Process client user login
-
-     # noqa: E501
-
-    :param request:
-    :type request: web.Request
-    """
-
-    state = str(uuid4())
-    _state_cache.append(state)
-
-    login_url = \
-        resources['oauth2']['host'] + \
-        '/login?response_type=code' + \
-        '&state=' + state + \
-        '&client_id=' + \
-        resources['oauth2']['client_id'] + \
-        '&redirect_uri=' + \
-        resources['oauth2']['site_uri'] + \
-        resources['oauth2']['login_callback']
-
-    raise web.HTTPFound(login_url)
-
-
-async def kge_logout(request: web.Request, session_id: str = None):  # noqa: E501
-    """Process client user logout
-
-     # noqa: E501
-
-    :param request:
-    :type request: web.Request
-    :param session_id:
-    :type session_id: str
-    """
-
-    # invalidate session here?
-    if valid_session(session_id):
-
-        delete_session(session_id)
-
-        # ...then redirect to signal logout at the Oauth2 host
-        logout_url = \
-            resources['oauth2']['host'] + \
-            '/logout?client_id=' + \
-            resources['oauth2']['client_id'] + \
-            '&logout_uri=' + \
-            resources['oauth2']['site_uri']
-
-        raise web.HTTPFound(logout_url)
-    else:
-        # redirect to unauthenticated landing page for login
-        raise web.HTTPFound(LANDING)
-
 
 #############################################################
 # Provider Controller Handler
@@ -226,6 +56,7 @@ async def kge_logout(request: web.Request, session_id: str = None):  # noqa: E50
 #
 # from ..kge_handlers import kge_access
 #############################################################
+
 
 # TODO: get file out from timestamped folders 
 async def kge_access(request: web.Request, kg_name: str, session_id: str) -> web.Response:  # noqa: E501
