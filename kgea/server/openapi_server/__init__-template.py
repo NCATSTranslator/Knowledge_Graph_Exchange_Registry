626bac193d97c50f0bba9ebc4b851c94729b050c
Richard Bruskiewich <richard@starinformatics.com>
2021-03-09 17:24:43 -0800

    Deleting some deprecated code plus fix some docs

--- kgea/server/openapi_server/__init__-template.py
+++ kgea/server/openapi_server/__init__-template.py
@@ -1,8 +1,16 @@
 import os
 import connexion
 import aiohttp_cors
-import aiohttp_jinja2
-import jinja2
+
+import aiohttp_session
+import aiomcache
+from aiohttp_session.memcached_storage import MemcachedStorage
+
+# TODO: configure the session management storage Docker-aware
+MEMCACHED_SERVICE = "localhost"
+
+# Docker Service container name
+# MEMCACHED_SERVICE = "memcached"
 
 
 def main():
@@ -11,6 +19,7 @@ def main():
         }
     specification_dir = os.path.join(os.path.dirname(__file__), 'openapi')
     app = connexion.AioHttpApp(__name__, specification_dir=specification_dir, options=options)
+
     app.add_api('openapi.yaml',
                 arguments={'title': 'OpenAPI for the NCATS Biomedical Translator Knowledge Graph EXchange (KGE) Archive'},
                 pythonic_params=True,
@@ -28,5 +37,9 @@ def main():
     # Register all routers for CORS.
     for route in list(app.app.router.routes()):
         cors.add(route)
+
+    mc = aiomcache.Client(MEMCACHED_SERVICE, 11211)
+    storage = MemcachedStorage(mc)
+    aiohttp_session.setup(app, storage)
 
     app.run(port=8080)
