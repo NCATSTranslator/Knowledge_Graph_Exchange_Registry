3d3a8c65d715e471c280a164539d555dd3f23533
Richard Bruskiewich <richard@starinformatics.com>
2021-03-26 13:51:07 -0700

    fix handler parameter names: kg_name => kg_id

--- kgea/server/web_services/controllers/provider_controller.py
+++ kgea/server/web_services/controllers/provider_controller.py
@@ -3,13 +3,13 @@ from aiohttp import web
 from ..kgea_handlers import kge_access
 
 
-async def access(request: web.Request, kg_name: str) -> web.Response:
+async def access(request: web.Request, kg_id: str) -> web.Response:
     """Get KGE File Sets
 
     :param request:
     :type request: web.Request
-    :param kg_name: Name label of KGE File Set, the knowledge graph for which data files are being accessed
-    :type kg_name: str
+    :param kg_id: Name label of KGE File Set, the knowledge graph for which data files are being accessed
+    :type kg_id: str
 
     """
-    return await kge_access(request, kg_name)
+    return await kge_access(request, kg_id)
