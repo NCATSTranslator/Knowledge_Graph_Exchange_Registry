9e32392a253b9b1227f097ea428de65b328e8b11
Richard Bruskiewich <richard@starinformatics.com>
2021-04-01 10:11:52 -0700

    stub iteration on cognito elaboration

--- kgea/server/web_ui/kgea_ui_handlers.py
+++ kgea/server/web_ui/kgea_ui_handlers.py
@@ -1,5 +1,5 @@
 from os import getenv
-from typing import List
+from typing import List, Dict
 from uuid import uuid4
 from datetime import datetime
 
@@ -122,6 +122,60 @@ async def get_kge_home(request: web.Request, uid: str = None) -> web.Response:  
         await redirect(request, LANDING)
 
 
+async def get_authorization(code: str) -> Dict:
+    logger.debug("Entering _get_authorization(code: "+code+")")
+    user_attributes: Dict = dict()
+    #
+    # See https://aws.amazon.com/blogs/mobile/how-to-use-cognito-pre-token-generators-to-customize-claims-in-id-tokens/
+    #
+    # got the authorization code Query parameter, the next step is to exchange it for user pool tokens. The exchange occurs by submitting a POST request with code Query parameter, client Id and Authorization Header like below.
+    #
+    # # HTTP Request (including valid token with "email" scope)
+    # $ curl -X POST \
+    #   'https://<Cognito User Pool Domain>/oauth2/token?
+    #   grant_type=authorization_code&
+    #   code=8a24d2df-07b9-41e1-bb5c-c269e87838df&
+    #   redirect_uri=http://localhost&
+    #   client_id=55pb79dl8gm0i1ho9hdrXXXXXX&scope=openid%20email' \
+    #   -H 'Accept-Encoding: gzip, deflate' \
+    #   -H 'Authorization: Basic NTVwYj......HNXXXXXXX' \
+    #   -H 'Content-Type: application/x-www-form-urlencoded'
+    #
+    # Ssh
+    #
+    # We would need to set the Authorization header for this request as Basic BASE64(CLIENT_ID:CLIENT_SECRET), where BASE64(CLIENT_ID:CLIENT_SECRET) is the base64 representation of the app client ID and app client secret, concatenated with a colon.
+    #
+    # Once the POST Request is successful we should get a response with id_token, access_token and refresh_token.
+    #
+    # {
+    #     "id_token":"XXXXXXx.....XXXXXXX",
+    #     "access_token":"XXXXXXx.....XXXXXXX",
+    #     "refresh_token":"XXXXXXx.....XXXXXXX",
+    #     "expires_in": 3600,
+    #     "token_type": "Bearer"
+    # }
+    #
+    # JSON
+    #
+    # Decoding the JWT ID Token will yield the following results with custom claim pet_preference added to the Id Token.
+    #
+    # {
+    #     "at_hash": "4FNVgmQsm5m_h9VC_OFFuQ",
+    #     "sub": "472ff4cd-9b09-46b5-8680-e8c5d6025d38",
+    #     "aud": "55pb79dl8gm0i1ho9hdre91r3k",
+    #     "token_use": "id",
+    #     "auth_time": 1576816174,
+    #     "iss": "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_qyS1sSLiQ",
+    #     "pet_preference": "dogs",
+    #     "cognito:username": "test-user",
+    #     "exp": 1576819774,
+    #     "iat": 1576816174,
+    #     "email": "test-user@amazon.com“
+    # }
+    #
+    return user_attributes
+
+
 # hack: short term state dictionary
 _state_cache = []
 
@@ -132,11 +186,18 @@ async def kge_client_authentication(request: web.Request):
     :param request:
     :type request: web.Request
     """
-    code = request.query['code']
-    state = request.query['state']
 
-    # Establish session here if there
-    # is a valid access code & state variable?
+    error = request.query.get('error', default='')
+    if error:
+        error_description = request.query.get('error_description', default='')
+        await report_error(request, "User not authenticated. Reason: " + str(error_description))
+    
+    code = request.query.get('code', default='')
+    state = request.query.get('state', default='')
+    if not (code and state):
+        await report_error(request, "User not authenticated. Reason: no authorization code returned?")
+    
+    # Establish session here if there is a valid access code & state variable?
     if state in _state_cache:
 
         # state 'secrets' are only got for one request
@@ -145,14 +206,11 @@ async def kge_client_authentication(request: web.Request):
         # now, check the returned code for authorization
         if code:
 
-            # Let everything through for the initial iteration
-            # Need to tie into AWS Cognito validation
-            # TODO: check AWS authorization token
-            authenticated = True  # stub implementation of validation
+            authenticated_user = get_authorization(code)
 
-            if authenticated:
+            if authenticated_user:
     
-                await initialize_user_session(request)
+                await initialize_user_session(request, authenticated_user)
                 
                 # if active session and no exception raised, then
                 # redirect to the home page, with a session cookie
