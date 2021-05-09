74dc6614c2c2f6bf2fb372893400512030b00bd3
Richard Bruskiewich <richard@starinformatics.com>
2021-05-08 21:28:56 -0700

    Code generated related to KgeFileSetStatusCode model

--- kgea/server/web_services/models/kge_file_set_status.py
+++ kgea/server/web_services/models/kge_file_set_status.py
@@ -4,6 +4,7 @@ from typing import List
 
 from kgea.server.web_services.models.base_model_ import Model
 from kgea.server.web_services.models.kge_file import KgeFile
+from kgea.server.web_services.models.kge_file_set_status_code import KgeFileSetStatusCode
 from kgea.server.web_services import util
 
 
@@ -13,27 +14,31 @@ class KgeFileSetStatus(Model):
     Do not edit the class manually.
     """
 
-    def __init__(self, kg_id: str=None, kg_version: str=None, files: List[KgeFile]=None):
+    def __init__(self, kg_id: str=None, kg_version: str=None, status: KgeFileSetStatusCode=None, files: List[KgeFile]=None):
         """KgeFileSetStatus - a model defined in OpenAPI
 
         :param kg_id: The kg_id of this KgeFileSetStatus.
         :param kg_version: The kg_version of this KgeFileSetStatus.
+        :param status: The status of this KgeFileSetStatus.
         :param files: The files of this KgeFileSetStatus.
         """
         self.openapi_types = {
             'kg_id': str,
             'kg_version': str,
+            'status': KgeFileSetStatusCode,
             'files': List[KgeFile]
         }
 
         self.attribute_map = {
             'kg_id': 'kg_id',
             'kg_version': 'kg_version',
+            'status': 'status',
             'files': 'files'
         }
 
         self._kg_id = kg_id
         self._kg_version = kg_version
+        self._status = status
         self._files = files
 
     @classmethod
@@ -90,6 +95,27 @@ class KgeFileSetStatus(Model):
         """
 
         self._kg_version = kg_version
+
+    @property
+    def status(self):
+        """Gets the status of this KgeFileSetStatus.
+
+
+        :return: The status of this KgeFileSetStatus.
+        :rtype: KgeFileSetStatusCode
+        """
+        return self._status
+
+    @status.setter
+    def status(self, status):
+        """Sets the status of this KgeFileSetStatus.
+
+
+        :param status: The status of this KgeFileSetStatus.
+        :type status: KgeFileSetStatusCode
+        """
+
+        self._status = status
 
     @property
     def files(self):
