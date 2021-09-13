d5f2af29ccae8c3a1050654a779dfa0867063b41
Kenneth <kenneth.bruskiewicz@gmail.com>
2021-09-13 14:27:03 -0700

    archiving scripts first draft

--- upload_archiver.sh
+++ upload_archiver.sh
@@ -25,22 +25,23 @@ output=$(aws s3 ls $s3  | awk '{print $4}')
 # iterate over file keys
 for file in $output
 do
-  echo $file
+  echo "$file"
   # example command:
   # aws s3 cp s3://kgea-test-bucket/kge-data/sri-reference-graph/1.0/content_metadata.json . | tar rf sri-reference-graph.1.0.tar content_metadata.json
   # use `rf` for tar to create if not exists, append files if existing
   aws s3 cp $s3"$file" . | tar rf "$tarfile_dir" "$file"
 done
-#
+
 ## after archiving all of the files, compress them
 ## note that `z` is incompatible with `r`, so we couldn't compress as we go
-#tar czf "$tarfile_dir"'.gz' "$tarfile_dir"
-#
+tar czf "$tarfile_dir"'.gz' "$tarfile_dir"
+
 ## copy the new archive file to s3
-#aws s3 cp "$tarfile_dir"'.gz' $s3'archive/'$tarfile_name'.gz'
-#
+aws s3 cp "$tarfile_dir"'.gz' $s3'archive/'$tarfile_name'.gz'
+
 ## cleanup the files
 ## both the local copies of the tar file and gz files
-#rm "$tarfile_dir"'.gz' "$tarfile_dir"
+rm "$tarfile_dir"'.gz' "$tarfile_dir"
+
 #
-#echo "Done"
+echo "Done"
