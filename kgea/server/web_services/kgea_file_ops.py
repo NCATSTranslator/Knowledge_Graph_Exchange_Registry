740f8dfe80be6814f2a3fa122a9102f1937cf392
Richard Bruskiewich <richard@starinformatics.com>
2021-09-14 15:12:38 -0700

    fix agg_path in unit test

--- kgea/server/web_services/kgea_file_ops.py
+++ kgea/server/web_services/kgea_file_ops.py
@@ -934,7 +934,7 @@ def test_aggregate_files():
         print_error_trace("Error while unpacking archive?: " + str(e))
         return False
     
-    assert(agg_path == f"{target_folder}/nodes.tsv")
+    assert(agg_path == f"s3://{TEST_BUCKET}/{target_folder}/nodes.tsv")
     
     return True
 
@@ -962,7 +962,7 @@ def test_huge_aggregate_files():
         print_error_trace("Error while unpacking archive?: " + str(e))
         return False
 
-    assert (agg_path == f"{target_folder}/nodes_plus_edges.tsv")
+    assert (agg_path == f"s3://{TEST_BUCKET}/{target_folder}/nodes_plus_edges.tsv")
     
     return True
 
