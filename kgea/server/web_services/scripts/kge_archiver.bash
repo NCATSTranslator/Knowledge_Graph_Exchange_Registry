7ba24869e3481d84bc56fefdb05ac3f2ca175a53
Kenneth <kenneth.bruskiewicz@gmail.com>
2021-09-14 17:34:39 -0700

    minor tweaks to bash script

--- kgea/server/web_services/scripts/kge_archiver.bash
+++ kgea/server/web_services/scripts/kge_archiver.bash
@@ -8,12 +8,18 @@
 # $KGE_BUCKET - S3 bucket source where the file set is located
 # $KGE_ROOT_DIRECTORY - Root S3 folder in the bucket, containing the knowledge graph -> file set
 #
+gzip=gzip
+# If multicore CPU's are available and compression speed is desired,
+# the 'parallel gz' (pigz; https://zlib.net/pigz/) could be used
+# gzip=pigz
+
 usage () {
     echo
     echo "Usage:"
     echo
     echo "./upload_archiver.bash <Knowledge Graph Identifier> <File Set Version>"
-    exit -1
+#    exit -1  bash exits 0-255
+    exit 1
 }
 
 if [ -z "$KGE_BUCKET" ]
@@ -21,7 +27,8 @@ then
     echo
     echo "Please set the \$KGE_BUCKET environment variable"
     echo "(S3 bucket source where the file set is located)"
-    exit -2
+#    exit -2  bash exits 0-255
+    exit 2
 fi
 
 if [ -z "$KGE_ROOT_DIRECTORY" ]
@@ -29,7 +36,8 @@ then
     echo
     echo "Please set the \$KGE_ROOT_DIRECTORY environment variable"
     echo "(Root S3 folder in the bucket, containing the knowledge graph -> file set)"
-    exit -3
+#    exit -3  bash exits 0-255
+    exit 3
 fi
 
 if [ -z "$1" ]
@@ -37,7 +45,7 @@ then
     usage
 else
     # Knowledge Graph Identifier
-    kgid=$1
+    knowledge_graph=$1
 fi
 
 if [ -z "$2" ]
@@ -56,56 +64,57 @@ else
         aws=`which aws`
 fi
 
-echo "Beginning archiving of file set version $version of knowledge graph $kgid"
+echo
+echo "Beginning archiving of file set version '$version' of knowledge graph '$kgid'"
 echo
 
-# Knowledge Graph S3 folder
-knowledge_graph=$KGE_BUCKET'/'$KGE_ROOT_DIRECTORY'/'$kgid
-
 # Folder of given versioned file set of the Knowledge Graph
-file_set=$knowledge_graph'/'$version
+file_set=$knowledge_graph/$version
 
 # Full S3 object key to the file set folder
-s3='s3://'$file_set
+s3=s3://$KGE_BUCKET/$KGE_ROOT_DIRECTORY/$file_set/archive
 
 # File Set tar archive
-tarfile=$kgid"_$version.tar"
+tarfile=$knowledge_graph'_'$version.tar
 
-echo kgid = $kgid
-echo version = $version
-echo bucket = $KGE_BUCKET
-echo root = $KGE_ROOT_DIRECTORY
-echo knowledge_graph = $knowledge_graph
-echo file_set = $file_set
-echo s3 = $s3
-echo tarfile = $tarfile
+# echo Knowledge Graph File Set = $knowledge_graph $version
+# echo s3 archive root = $s3
+# echo archive = $s3/$tarfile.gz
 
 # use awk to get the keys for files
-output=`$aws s3 ls "$s3/"| awk '{print $4}'`
+# output=`$aws s3 ls "$s3/"| awk '{print $4}'`
+output=("provider.yaml" "file_set.yaml" "content_metadata.json" "nodes.tsv" "edges.tsv")
 
-# iterate over file keys
-for file in $output
+# iterate over files
+echo
+echo Retrieve and tar files:
+for file in $"{output[@]}";  # "double quote an array to prevent resplitting elements"  https://github.com/koalaman/shellcheck/wiki/SC2068
 do
-  # remove trailing whitespace such as carriage returns
-  file=`echo "$file" | sed 's/\s\$//g'`
-  echo "$file"
-  # example command:
-  # aws s3 cp s3://kgea-test-bucket/kge-data/sri-reference-graph/1.0/content_metadata.json . | tar rf sri-reference-graph.1.0.tar content_metadata.json
-  # use `rf` for tar to create if not exists, append files if existing
-  $aws s3 cp $s3"/$file" - | tar rf "$tarfile" -
+  $aws s3 cp $s3/$file .
+  if [ $? -eq 0 ] && [ -f $file ]; then
+     echo "- $file: archived!"
+     # use `rf` for tar to create if not exists, append files if existing
+     tar rf $tarfile $file
+     rm $file
+  else
+     echo "- $file: unavailable for archiving?"
+  fi
 done
 
 ## after archiving all of the files, compress them
 ## note that `z` is incompatible with `r`, so we couldn't compress as we go
 # tar czf "$tarfile.gz" "$tarfile"
-gzip "$tarfile"
+$gzip $tarfile
 
 ## copy the new archive file to s3
-$aws s3 cp "$tarfile.gz" "$s3/archive/$tarfile.gz"
+$aws s3 cp $tarfile.gz $s3/$tarfile.gz
 
 ## cleanup the files
 ## both the local copies of the tar file and gz files
-rm "$tarfile.gz"
+rm $tarfile.gz
 
 echo
 echo "...done archiving"
+
+# signal of success to other processes
+exit 0;
