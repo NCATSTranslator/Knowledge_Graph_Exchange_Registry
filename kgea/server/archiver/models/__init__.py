806837e105f8b85aee5306e609949a908a7e4c69
Richard Bruskiewich <richard@starinformatics.com>
2021-11-15 11:22:29 -0800

    stubs regenerated for modified StatusToken->ProcessStatus model change

--- kgea/server/archiver/models/__init__.py
+++ kgea/server/archiver/models/__init__.py
@@ -5,5 +5,5 @@ from kgea.server.archiver.models.kge_file import KgeFile
 from kgea.server.archiver.models.kge_file_set_metadata import KgeFileSetMetadata
 from kgea.server.archiver.models.kge_file_set_status_code import KgeFileSetStatusCode
 from kgea.server.archiver.models.kge_file_type import KgeFileType
+from kgea.server.archiver.models.process_status import ProcessStatus
 from kgea.server.archiver.models.process_status_code import ProcessStatusCode
-from kgea.server.archiver.models.status_token import StatusToken
