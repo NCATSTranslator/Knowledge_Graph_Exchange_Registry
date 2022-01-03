99d928355b1ebb68143598b1aee634ca06ad71d4
Richard Bruskiewich <richard@starinformatics.com>
2022-01-02 16:52:56 -0800

    trying to make metadata test more verbose?

--- kgea/tests/unit/test_kgea_config.py
+++ kgea/tests/unit/test_kgea_config.py
@@ -1,13 +1,17 @@
 from typing import Dict
 from sys import stderr
-import pytest
 from pprint import pprint
 from kgea.aws.ec2 import get_ec2_instance_metadata
 
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel("DEBUG")
+
 
 def test_instance_metadata():
+    logger.debug("Testing Instance Metadata Access")
     metadata: Dict = get_ec2_instance_metadata()
     assert "instanceId" in metadata
     assert "region" in metadata
     assert "availabilityZone" in metadata
-    pprint(metadata,  stream=stderr)
+    pprint(metadata)
