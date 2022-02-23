8d91b720fc8ea21f6c21d3100ef1761a68ad6dcb
Richard Bruskiewich <richard@starinformatics.com>
2022-02-23 14:27:43 -0800

    moving  ebs unit tests into archiver; clean up archiver test a bit

--- kgea/server/archiver/tests/test_archiver.py
+++ kgea/server/archiver/tests/test_archiver.py
@@ -1,27 +1,24 @@
 from sys import stderr
 from typing import List
 
+import pytest
+
 from kgea.server.archiver.kge_archiver_util import compress_fileset, logger
 from kgea.server.tests import TEST_KG_ID, TEST_FS_VERSION, TEST_BUCKET
 from kgea.server.tests.unit.test_kgea_file_ops import logger
 
 
+@pytest.mark.asyncio
 async def test_compress_fileset():
-    try:
-        s3_archive_key: str = await compress_fileset(
-            kg_id=TEST_KG_ID,
-            version=TEST_FS_VERSION,
-            bucket=TEST_BUCKET,
-            root='kge-data'
-        )
-        logger.info(f"test_compress_fileset(): s3_archive_key == {s3_archive_key}")
-        assert (s3_archive_key == f"s3://{TEST_BUCKET}/kge-data/{TEST_KG_ID}/{TEST_FS_VERSION}"
-                                  f"/archive/{TEST_KG_ID + '_' + TEST_FS_VERSION}.tar.gz")
-    except Exception as e:
-        logger.error(e)
-        assert False
-
-
+    s3_archive_key: str = await compress_fileset(
+        kg_id=TEST_KG_ID,
+        version=TEST_FS_VERSION,
+        bucket=TEST_BUCKET,
+        root='kge-data'
+    )
+    logger.info(f"test_compress_fileset(): s3_archive_key == {s3_archive_key}")
+    assert (s3_archive_key == f"s3://{TEST_BUCKET}/kge-data/{TEST_KG_ID}/{TEST_FS_VERSION}"
+                              f"/archive/{TEST_KG_ID + '_' + TEST_FS_VERSION}.tar.gz")
 
 
 # TODO: more complete KGX validator test
@@ -36,4 +33,5 @@ def test_kgx_data_validator():
 
     if errors:
         logger.error("test_contents_data_validator() errors: " + str(errors))
-    return not errors
+
+    assert not errors
